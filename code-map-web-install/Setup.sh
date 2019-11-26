585fd999965dc0755dc2deb89a43c4659d52fcbf
akshatsinghkaushik <akshatsingh99@gmail.com>
2019-11-26 15:12:47 +1100

    Fixed Setup.sh

--- code-map-web-install/Setup.sh
+++ code-map-web-install/Setup.sh
@@ -1,14 +1,14 @@
 sudo apt update && sudo apt upgrade
-bash ../codemap-setup-subscripts/setupBasic.sh
-bash ../codemap-setup-subscripts/setupPip.sh
-source ../codemap-setup-subscripts/setupNvm.sh
-bash ../codemap-setup-subscripts/setupPygraphviz.sh
-bash ../codemap-setup-subscripts/setupVscodeEssential.sh
-bash ../codemap-setup-subscripts/setupYarn.sh
-bash ../codemap-setup-subscripts/setupNodemon.sh
+bash codemap-setup-subscripts/setupBasic.sh
+bash codemap-setup-subscripts/setupPip.sh
+source codemap-setup-subscripts/setupNvm.sh
+bash codemap-setup-subscripts/setupPygraphviz.sh
+bash codemap-setup-subscripts/setupVscodeEssential.sh
+bash codemap-setup-subscripts/setupYarn.sh
+bash codemap-setup-subscripts/setupNodemon.sh
 
-source ../codemap-setup-subscripts/githubdownload.sh
-bash ../codemap-setup-subscripts/FixWatchMax.sh
-bash ../codemap-setup-subscripts/setCodeMapExtension.sh
+source codemap-setup-subscripts/githubdownload.sh
+bash codemap-setup-subscripts/FixWatchMax.sh
+bash codemap-setup-subscripts/setCodeMapExtension.sh
 
 sudo apt autoremove -y
