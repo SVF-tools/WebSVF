58ad2615af7fc438775374fd14c0d2e0ee09a5ad
Akshat Singh <akshatsingh99@gmail.com>
2019-12-05 21:50:05 +1100

    Changed README and Code Reduction

--- web-svf-install/codemap-setup-subscripts/setupPip.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install -y python-pip
-sudo apt install -y python3-pip
--- web-svf-install/codemap-setup-subscripts/setupPygraphviz.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install -y python-pip
-pip install graphviz
-sudo apt install -y graphviz
-sudo apt install -y libgraphviz-dev
-pip install pygraphviz
