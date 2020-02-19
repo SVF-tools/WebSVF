6641977b854444510b4b94df4e6429110a2260e7
Akshat Singh <akshatsingh99@gmail.com>
2020-02-19 13:45:13 +1100

    Landing Page Template Completed w/ working demo

--- landingPage.js
+++ landingPage.js
@@ -20,9 +20,9 @@ const make_pill = (index, name) => {
     var name_noDot = name.replace('.','');
 
     if (index == 0)
-    output = '\n\n\t\t\t\t\t\t\t\t\t<div class="tab-pane fade shadow rounded bg-white show active p-5" id="v-pills-' + name_noDot + '" role="tabpanel" aria-labelledby="v-pills-' + name_noDot + '-tab">\n\t\t\t\t\t\t\t\t\t\t\t<h4 class="font-italic mb-4"><a href="#" target="_blank">' + name + '</a></h4>\n\t\t\t\t\t\t\t\t\t\t\t<p class="font-italic text-muted mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\n\t\t\t\t\t\t\t\t\t</div>\n\n\t\t\t\t\t\t\t\t\t';
+    output = '\n\n\t\t\t\t\t\t\t\t\t<div class="tab-pane fade shadow rounded bg-white show active p-5" id="v-pills-' + name_noDot + '" role="tabpanel" aria-labelledby="v-pills-' + name_noDot + '-tab">\n\t\t\t\t\t\t\t\t\t\t\t<h4 class="font-italic mb-4"><a id="'+ index + '*!*' + name +'" class="fileRepLink" href="#" target="_blank">' + name + '</a></h4>\n\t\t\t\t\t\t\t\t\t\t\t<pre id="errors-'+ name_noDot +'" style="overflow-x: hidden; color:green; font-size: 1rem; font-weight: bold;"></pre>\n\t\t\t\t\t\t\t\t\t</div>\n\n\t\t\t\t\t\t\t\t\t';
     else
-    output = '<div class="tab-pane fade shadow rounded bg-white p-5" id="v-pills-' + name_noDot + '" role="tabpanel" aria-labelledby="v-pills-' + name_noDot + '-tab">\n\t\t\t\t\t\t\t\t\t\t\t<h4 class="font-italic mb-4"><a href="#" target="_blank">' + name + '</a></h4>\n\t\t\t\t\t\t\t\t\t\t\t<p class="font-italic text-muted mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\n\t\t\t\t\t\t\t\t\t</div>\n\n\t\t\t\t\t\t\t\t\t';
+    output = '<div class="tab-pane fade shadow rounded bg-white p-5" id="v-pills-' + name_noDot + '" role="tabpanel" aria-labelledby="v-pills-' + name_noDot + '-tab">\n\t\t\t\t\t\t\t\t\t\t\t<h4 class="font-italic mb-4"><a id="'+ index + '*!*' + name +'" class="fileRepLink" href="#" target="_blank">' + name + '</a></h4>\n\t\t\t\t\t\t\t\t\t\t\t<pre id="errors-'+ name_noDot +'" style="overflow-x: hidden; color:green; font-size: 1rem; font-weight: bold;"></pre>\n\t\t\t\t\t\t\t\t\t</div>\n\n\t\t\t\t\t\t\t\t\t';
 
     return output;
 }
@@ -45,16 +45,23 @@ const gen_landing_page = () => {
     const $ = jquery(dom.window);
     $('#v-pills-tab').html('');
     $('#v-pills-tabContent').html('');
+    
 
     for(var i = 0; i<=jsonReport_length; ++i){
         //console.log(jsonReport.bugreport[i].id);
 
         var fileName = jsonReport.bugreport[i].FileName;
         var filePath = jsonReport.bugreport[i].FilePath;
+        var fileErrors = jsonReport.bugreport[i].Errors;
+        
 
         $('#v-pills-tab').append(make_pillTab(i,fileName));
 
         $('#v-pills-tabContent').append(make_pill(i,fileName));
+
+        //console.log(jsonReport.bugreport[i].Errors);
+
+        $('#v-pills-tabContent #errors-'+fileName.replace('.','')).html(JSON.stringify(fileErrors, undefined, 4));
 
 
 
