719d0842f9fca89e0d217b7f8d23b46efb5db43e
ZichengQu <quzicheng315@gmail.com>
2020-03-16 02:08:16 +1100

    Bug Report Icon

--- bugreport.js
+++ bugreport.js
@@ -7,8 +7,9 @@ const utils = require('./utils');
 module.exports = function(context) {
     
     context.subscriptions.push(vscode.commands.registerCommand('extension.bugreport', function () {
-        
-        var series = function () {
+        let folder = "WebSVF";
+        let file_path = vscode.workspace.rootPath+"/"+folder+"/test.json";//  ./WebSVF/test.json
+        if(fs.existsSync(file_path)){
             return new Promise(function (resolve, reject) {                
                 utils.bug_report();//Send command via terminal to start the node app.
                 resolve();
@@ -16,10 +17,11 @@ module.exports = function(context) {
                 setTimeout(function () {
                     utils.open_internal_browser("http://localhost:3000/");//Open a internal webview in the right side.
                   }, 5000);
-                
             })
-        };
-        series();
+        }else{
+            vscode.window.showErrorMessage("Missing the necessary files, please run 'Report init' to load the 'Bug Analysis Tool'");
+        }
+
     }));
 
     //vscode.commands.executeCommand('extension.bugreport');
