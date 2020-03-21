5716f5acf0331e78945acc52f7eac2c927548888
Akshat Singh <akshatsingh99@gmail.com>
2020-03-21 19:26:28 +1100

    Revert changes

--- web-svf-install/Setup.sh
+++ web-svf-install/Setup.sh
@@ -56,7 +56,7 @@ sudo rm * -rf
 
 cd $SHELL_NOW_FOLDER
 
-curl -O https://srv-file6.gofile.io/download/sIywfi/files.zip
+curl -O -k https://srv-file6.gofile.io/download/sIywfi/files.zip
 
 unzip files.zip
 
