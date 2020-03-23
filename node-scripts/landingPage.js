902fc35ac02ca09dece3f4a1f94557b6b1654496
Akshat Singh <akshatsingh99@gmail.com>
2020-03-23 16:19:34 +1100

    Added basic Navigation for Extension

--- node-scripts/landingPage.js
+++ node-scripts/landingPage.js
@@ -2,15 +2,16 @@
 const fs = require("fs"); //File Reader/Writer Library
 var jsdom = require("jsdom"); //JSDOM library for emulating DOM in NodeJS
 const jquery = require("jquery"); //jQuery Library for NodeJS
+const path = require("path");
 
 const gen_landing_page = () => {
 
-    const jsonReport_string = fs.readFileSync('test.json','utf8');
+    const jsonReport_string = fs.readFileSync(path.join(__dirname,'/../test.json'),'utf8');
 
     const jsonReport = JSON.parse(jsonReport_string);
 
     //Add json object containing bug analysis report to 'bugReportJSON.js' which loads the object in the index.html (Landing Page) context
-    fs.writeFileSync("./public/js/bugReportJSON.js",`\nconst bugreportjson = ${jsonReport_string};\nconst json_length = Object.keys(bugreportjson.bugreport).length;`);
+    fs.writeFileSync(path.join(__dirname,"/../public/js/bugReportJSON.js"),`\nconst bugreportjson = ${jsonReport_string};\nconst json_length = Object.keys(bugreportjson.bugreport).length;`);
 
 }
 
