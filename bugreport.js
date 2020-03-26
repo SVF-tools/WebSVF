99607add4d818c230f9c18ba307a15db722d01ba
ZichengQu <quzicheng315@gmail.com>
2020-03-26 19:20:24 +1100

    Download zip instead of git clone, unzip and remove zip; Make adjustments for Node app updates

--- bugreport.js
+++ bugreport.js
@@ -7,8 +7,9 @@ const utils = require('./utils');
 module.exports = function(context) {
     
     context.subscriptions.push(vscode.commands.registerCommand('extension.bugreport', function () {
-        let folder = "WebSVF";
-        let file_path = vscode.workspace.rootPath+"/"+folder+"/test.json";//  ./WebSVF/test.json
+        let folder = "/WebSVF-bug-report-fe/";
+        let file_path = vscode.workspace.rootPath+folder+"Bug-Analysis-Report.json";//  workspace/bug-report-fe/WebSVF-bug-report-fe/Bug-Analysis-Report.json
+        console.log(file_path);
         if(fs.existsSync(file_path)){
             return new Promise(function (resolve, reject) {                
                 utils.bug_report();//Send command via terminal to start the node app.
@@ -21,8 +22,6 @@ module.exports = function(context) {
         }else{
             vscode.window.showErrorMessage("Missing the necessary files, please run 'Report init' to load the 'Bug Analysis Tool'");
         }
-
     }));
-
     //vscode.commands.executeCommand('extension.bugreport');
 };
