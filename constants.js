6b6cb1c8db9121a709c144a0ac12c8464cbc8909
ZichengQu <quzicheng315@gmail.com>
2020-03-27 22:50:36 +1100

    Extract the public variables into a separate module, adding more documentation comments in detail.

---
+++ constants.js
@@ -0,0 +1,13 @@
+const vscode = require('vscode');
+
+const constants = {
+    //Path
+    workspace: vscode.workspace.rootPath,
+    node_app: '.bug-report', //Node app folder
+    node_branch: '/WebSVF-bug-report-fe', //Node app branch name
+    workspace_json: '/Bug-Analysis-Report.json', //To test if the Bug-Analysis-Report.json exists in the workspace.
+    node_json: '/'+this.node_app + this.node_branch + this.workspace_json,
+    download_uri: 'https://github.com/SVF-tools/WebSVF/archive/bug-report-fe.zip' //Download the bug_report analysis tool as a zip from remote github
+}
+
+module.exports = constants;
