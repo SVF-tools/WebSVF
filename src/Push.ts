c6dbf8c3e46524cb6ae747bef324bae883550e03
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-04-10 13:06:44 +1000

    Pre-Merge commit

--- src/Push.ts
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-export abstract class Push {
-    abstract push(): any;
-}
---
+++ src/codemap_extension/src/model/ForceGraph3D/CommonInterface.ts
@@ -0,0 +1,18 @@
+"use strict";
+
+export interface ConfigPath {
+    [key: string]: string;
+    barConfigPath: string;
+    PanelConfigPath: string;
+    CommandConfigPath: string;
+}
+
+export enum SwitchBar {
+    on,
+    off,
+}
+
+export enum BarSituation {
+    waiting,
+    running,
+}
