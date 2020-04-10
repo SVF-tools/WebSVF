7e9aecb288aa4ca4646d71d11e4b3f935f19bd53
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-04-10 12:41:15 +1000

    Pre-merge changes

---
+++ src/codemap_extension/src/model/LoadMajorModule.ts
@@ -0,0 +1,6 @@
+"use strict";
+import * as ForceGraph3D from "./ForceGraph3D/A_ForceGraph3D";
+
+export function LoadMajorModule() {
+    ForceGraph3D.LoadModule_ForceGraph3D(); // Load 3d force graph module
+}
