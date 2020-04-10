42f711b4c00daf11945fe5301e033ff0479bf18d
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-04-10 13:49:25 +1000

    test

---
+++ src/codemap_extension/README.md
@@ -0,0 +1,10 @@
+# COMMAND
+## yarn go
+
+clear all build and then build all 
+
+
+## yarn cl
+
+
+clear all build
---
+++ src/codemap_extension/react-part/scripts/build.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+// Disables code splitting into chunks
+// See https://github.com/facebook/create-react-app/issues/5306#issuecomment-433425838
+// It will let build/js 's files merge in one file
+
+const rewire = require("rewire");
+const defaults = rewire("react-scripts/scripts/build.js");
+let config = defaults.__get__("config");
+
+config.optimization.splitChunks = {
+  cacheGroups: {
+    default: false
+  }
+};
+
+config.optimization.runtimeChunk = false;
