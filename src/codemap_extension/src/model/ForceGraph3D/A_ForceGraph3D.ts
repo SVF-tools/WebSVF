3c8e5f6fb012ad14889e85cfcb697137c0c1a3c4
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-04-10 13:49:25 +1000

    test

---
+++ src/codemap_extension/src/model/ForceGraph3D/A_ForceGraph3D.ts
@@ -0,0 +1,52 @@
+"use strict";
+import * as path from "path";
+import * as vscode from "vscode";
+import { ActivateVscodeContext } from "../../components/ActivateVscodeContext";
+import { StatusBarForceGraph3DManager } from "./StatusBar";
+import { RegisterCommandForceGraph3DManager } from "./Command";
+import * as CommonInterface from "./CommonInterface";
+
+function getRootPath(): string {
+    return path.join(
+        ActivateVscodeContext.context.extensionPath,
+        "configuration",
+        "ForceGraph3D"
+    );
+}
+function getConfigPath(): CommonInterface.ConfigPath {
+    // Load configure info from Json file
+    const rootPath: string = getRootPath();
+    const BarConfigPath = path.join(rootPath, "BarConfig.json");
+    const ReactPanelConfigPath = path.join(rootPath, "ReactPanelConfig.json");
+    const CommandConfigPath = path.join(rootPath, "CommandConfig.json");
+
+    const configPath: CommonInterface.ConfigPath = {
+        barConfigPath: BarConfigPath,
+        PanelConfigPath: ReactPanelConfigPath,
+        CommandConfigPath: CommandConfigPath,
+    };
+    return configPath;
+}
+
+export function LoadModule_ForceGraph3D() {
+    const configPath: CommonInterface.ConfigPath = getConfigPath();
+    RegisterCommand_ForceGraph3D(configPath); // register command
+    CreateStatusBar_ForceGraph3D(configPath); // create status bar
+}
+
+// Register ForceGraph3D Command
+function RegisterCommand_ForceGraph3D(configPath: CommonInterface.ConfigPath) {
+    if (!RegisterCommandForceGraph3DManager.initial(configPath)) {
+        vscode.window.showErrorMessage(
+            "Register Command ForceGraph3DManager initial false."
+        );
+    }
+}
+// Create ForceGraph3D Status Bar
+function CreateStatusBar_ForceGraph3D(configPath: CommonInterface.ConfigPath) {
+    if (!StatusBarForceGraph3DManager.initial(configPath)) {
+        vscode.window.showErrorMessage(
+            "StatusBar ForceGraph3D Manager initial false."
+        );
+    }
+}
