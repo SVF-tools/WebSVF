8ed19c64237a27cc7954c1dfc5a2704d7ee6d9f8
spcidealacm <spcidealacm@gmail.com>
2020-06-01 14:03:26 +1000

    command entends

--- src/codemap_extension/src/model/ForceGraph3D/Command.ts
+++ src/codemap_extension/src/model/ForceGraph3D/Command.ts
@@ -9,16 +9,7 @@ import * as CommonInterface from "./CommonInterface";
 import { LineTagManager, LineTag } from "../../components/LineTag";
 import { LineTagForceGraph3DManager } from "./LineTag";
 
-export interface CommandInfo {
-    [key: string]: string;
-    key: string;
-    command: string;
-}
 
-export enum statusHighLight {
-    show,
-    hide,
-}
 
 export class RegisterCommandForceGraph3DManager {
     private static _rcf: RegisterCommandForceGraph3D | undefined = undefined;
@@ -34,41 +25,12 @@ export class RegisterCommandForceGraph3DManager {
     }
 }
 
-export class RegisterCommandForceGraph3D {
-    constructor(private coreData: CommonInterface.ConfigPath) {
-        this.pushCommand(this.registerCommand());
+export class RegisterCommandForceGraph3D extends CommonInterface.RegisterCommand{
+    constructor(protected coreData: CommonInterface.ConfigPath) {
+        super(coreData, "ForceGraph3D");
     }
 
-    private getCommandInfo(): CommandInfo {
-        const info = require(this.coreData.CommandConfigPath);
-        const commandInfo: CommandInfo = {
-            key: info["name"],
-            command: info["command"],
-        };
-        return commandInfo;
-    }
-
-    public getCommand(): string {
-        const commandInfo: CommandInfo = this.getCommandInfo();
-        return commandInfo.command;
-    }
-
-    public getKey(): string {
-        const commandInfo: CommandInfo = this.getCommandInfo();
-        return commandInfo.key;
-    }
-
-    private registerCommand(): vscode.Disposable {
-        return vscode.commands.registerCommand(this.getCommand(), () => {
-            this.mainFunc();
-        });
-    }
-
-    private pushCommand(registerCommand: vscode.Disposable) {
-        ActivateVscodeContext.context.subscriptions.push(registerCommand);
-    }
-
-    private mainFunc() {
+    protected mainFunc() {
         // vscode.window.showInformationMessage("3D FORCE GRAPH");
         if (StatusBarForceGraph3DManager.switchTurn()) {
             ActivateVscodeContext.activeEditor = vscode.window.activeTextEditor;
@@ -80,7 +42,7 @@ export class RegisterCommandForceGraph3D {
             CommonInterface.BarSituation.going;
         this.mainFunc();
     }
-    private loadWebPanel() {
+    protected loadWebPanel() {
         if (
             StatusBarForceGraph3DManager.switchBar ===
             CommonInterface.SwitchBar.on
@@ -95,22 +57,22 @@ export class RegisterCommandForceGraph3D {
                 this.coreData.PanelConfigPath,
                 newWebPanel
             );
-            this.changeConfigForHightLine(statusHighLight.show);
+            this.changeConfigForHightLine(CommonInterface.statusHighLight.show);
         } else {
             WebPanelForceGraph3DManager.deletePanel();
-            this.changeConfigForHightLine(statusHighLight.hide);
+            this.changeConfigForHightLine(CommonInterface.statusHighLight.hide);
         }
     }
 
-    private changeConfigForHightLine(status: statusHighLight) {
+    protected changeConfigForHightLine(status: CommonInterface.statusHighLight) {
         let settings = vscode.workspace.getConfiguration("codeMap");
         let Flag = 0;
         switch (status) {
-            case statusHighLight.show:
+            case CommonInterface.statusHighLight.show:
                 Flag = 1;
                 settings.update("ShowOrHide", "show").then(showInfo);
                 break;
-            case statusHighLight.hide:
+            case CommonInterface.statusHighLight.hide:
                 Flag = 2;
                 settings.update("ShowOrHide", "hide").then(showInfo);
                 break;
