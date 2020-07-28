ecb3f213ec799fbf91c798eb6aee0d162015b485
ZichengQu <quzicheng315@gmail.com>
2020-07-29 02:49:59 +1000

    The Webview is replaced with a third-party plug-in called 'Browser Preview' (the Webview is supported in the local VSCode, but the Code Server doesn't support opening links through the Webview). As for the existing issue, when the WebSVF Frontend extension is stopped, the Web interface generated by 'Browser Preview' cannot be closed in the meanwhile and can only be closed manually.

--- src/WebSVF-frontend-extension/src/menu.js
+++ src/WebSVF-frontend-extension/src/menu.js
@@ -9,7 +9,7 @@ var net = require('net')
 /**
  * @param {vscode.ExtensionContext} context
  */
-module.exports = function(context) {
+module.exports = function (context) {
 
     let timeInterval = null; //timeInterval for listening to when the port 3000 opened by the WebSVF frontend server.
 
@@ -24,37 +24,37 @@ module.exports = function(context) {
         let config_abspath = null; //The absolute path of config file in the WebSVF frontend server folder, which should be different in different OS.
 
         //Determine the Operating System, currently supports for Windows and Ubuntu.
-        if(os.platform() == "win32"){//This is for Windows.
+        if (os.platform() == "win32") {//This is for Windows.
             config_abspath = os.userInfo().homedir + path.sep + constants.node_app + constants.node_branch + constants.config_abspath;
-        }else{//This is for Ubuntu.
-            if(typeof(constants.workspace) == "undefined"){
+        } else {//This is for Ubuntu.
+            if (typeof (constants.workspace) == "undefined") {
                 vscode.window.showErrorMessage('No workplace opened, please open the target workplace!'); //Prompt users that no workplace opened yet.
                 return;
             }
-            let node_abspath = constants.workspace.substring(0,constants.workspace.indexOf(path.sep,6)+1) + constants.node_app; //The absolute path of the WebSVF frontend server.
+            let node_abspath = constants.workspace.substring(0, constants.workspace.indexOf(path.sep, 6) + 1) + constants.node_app; //The absolute path of the WebSVF frontend server.
             config_abspath = node_abspath + constants.config_abspath; //The absolute path of config file in the WebSVF frontend server folder.
         }
 
         //Perform different methods according to different circumstains.
-        if(status == "Initializing"){
+        if (status == "Initializing") {
             //Display a message box for user to choose whether cancel initializing.
             vscode.window.showInformationMessage("Please wait a moment for initializing, do you want to stop?", "Yes", "No").then(selection => {
-                if(selection == "Yes"){
+                if (selection == "Yes") {
                     stop();
                 }
             });
-        }else if(status == "Running"){
+        } else if (status == "Running") {
             //Display a message box for user to choose whether stop.
             vscode.window.showInformationMessage("Stop this 'Bug Analysis Tool'?", "Yes", "No").then(selection => {
-                if(selection == "Yes"){
+                if (selection == "Yes") {
                     stop();
                 }
             });
-        }else if (fs.existsSync(workspace_json) && fs.existsSync(config_abspath)) {
+        } else if (fs.existsSync(workspace_json) && fs.existsSync(config_abspath)) {
             analysis(); //Start analysis when both the workspace json and config file exist.
-        }else if(fs.existsSync(workspace_json) && !fs.existsSync(config_abspath)){
+        } else if (fs.existsSync(workspace_json) && !fs.existsSync(config_abspath)) {
             init(); //Start initializing the WebSVF frontend server if workspace json exists, but config files misses.
-        }else{
+        } else {
             //Display an error message box to the user when there is no test.json found.
             vscode.window.showErrorMessage('No Bug-Analysis-Report.json found in the workplace, the bug_report plugin cannot be actived!');
         }
@@ -63,7 +63,7 @@ module.exports = function(context) {
     /**
      * Initialization if files or tools missing.
      */
-    function init(){
+    function init() {
         utils.setStatusBar("Bug Analysis Tool: Initializing", "Red"); //Update the text and color of the status bar for Bug Analysis Tool button.
         utils.init(constants.download_uri);//Download the zip, uncompress the zip locally to a specific hidden directory (the directory is consistent with the address downloaded by the WebSVF Backend script), and then remove the zip.
     }
@@ -73,57 +73,59 @@ module.exports = function(context) {
      * Will create a server to listen to port 3000 and open an internal webview inside the vscode.
      * More functions please refer to the readme of this extension.
      */
-    function analysis(){
+    function analysis() {
         var server = net.createServer().listen(3000);//Create a server to listen to the port 3000.
 
         //It means that the WebSVF frontend server has been started successfully if the port 3000 has already been eaddrinused.
         server.on('error', function (err) {
             let flag = err.message.split(" ")[1];
-            flag = flag.substring(0, flag.length-1);
+            flag = flag.substring(0, flag.length - 1);
 
             if (flag === 'EADDRINUSE') { // The port has been occupied
                 utils.setStatusBar("Bug Analysis Tool: Running", "Red");//Update the status bar.
-                utils.open_internal_browser("http://localhost:3000/");//Open a internal webview in the right side inside the vscode if eaddrinused.
+                //utils.open_internal_browser("http://localhost:3000/");//Open a internal webview in the right side inside the vscode if eaddrinused.
+                vscode.commands.executeCommand('browser-preview.openPreview','http://localhost:3000');//// Call another extension 'Browser Preview' and listen to localhost with port 3000.
             }
         });
 
         server.on('listening', function () { // Execution of this code indicates that the port is not occupied
             server.close(); // Close the service
             utils.setStatusBar("Bug Analysis Tool: Running", "Red");
-            new Promise(function (resolve, reject) {                
+            new Promise(function (resolve, reject) {
                 utils.bug_report();//Send command via terminal to start the node app (WebSVF frontend server).
                 resolve(1);
             }).then(function () {
-                timeInterval = setInterval(function () {portIsOccupied(3000);}, 1000);//Check if the port 3000 has been started every one second.
+                timeInterval = setInterval(function () { portIsOccupied(3000); }, 1000);//Check if the port 3000 has been started every one second.
             });
-          });
+        });
     }
 
-    function stop(){
+    function stop() {
         utils.setStatusBar("Bug Analysis Tool", "White");//Update the status bar.
         utils.bug_report_stop();//Stop the webview.
     }
 
-    function portIsOccupied (port) {
+    function portIsOccupied(port) {
         // Create the service and listen on the port
         var server = net.createServer().listen(port);
-      
+
         server.on('listening', function () { // Execution of this code indicates that the port is not occupied
-          server.close(); // Close the service
-          //console.log('The port【' + port + '】 is available.');
+            server.close(); // Close the service
+            //console.log('The port【' + port + '】 is available.');
         });
-      
+
         server.on('error', function (err) {
 
             let flag = err.message.split(" ")[1];
-            flag = flag.substring(0, flag.length-1);
+            flag = flag.substring(0, flag.length - 1);
 
             if (flag === 'EADDRINUSE') { // The port has been occupied
                 //console.log('The port【' + port + '】 is occupied, please change other port.');
-                utils.open_internal_browser("http://localhost:3000/");//Open a internal webview in the right side inside the vscode.
+                //utils.open_internal_browser("http://localhost:3000/");//Open a internal webview in the right side inside the vscode.
+                vscode.commands.executeCommand('browser-preview.openPreview', 'http://localhost:3000'); // Call another extension 'Browser Preview' and listen to localhost with port 3000.
                 clearInterval(timeInterval);//Close the check every one second if the WebSVF frontend server has been started.
             }
         });
-      }
+    }
 
 };
