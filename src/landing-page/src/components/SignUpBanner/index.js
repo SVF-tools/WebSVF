d76c27d747beabd0c28ef139ec9f3759b961273a
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-08-29 02:01:21 +1000

    Minor Fixes to the landing-page

--- src/landing-page/src/components/SignUpBanner/index.js
+++ src/landing-page/src/components/SignUpBanner/index.js
@@ -6,29 +6,34 @@ const SignUpBanner = ({ heading, placeholder, buttonText }) => {
       <div className="overlay"></div>
       <div className="container">
         <div className="row">
-          <div className="col-xl-9 mx-auto">
+          <div className="col-xl-12 mx-auto">
             <h2 className="mb-4">{heading}</h2>
-          </div>
-          <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
             <form>
+              <button
+                type="submit"
+                onClick={() => (window.location.href = '#signupform')}
+                style={{ maxWidth: '250px' }}
+                className="btn btn-block btn-lg btn-primary mx-auto"
+              >
+                {buttonText}
+              </button>
+            </form>
+          </div>
+          <div className="col-md-10 col-lg-8 col-xl-3 mx-auto">
+            {/* <form>
               <div className="form-row">
                 <div className="col-12 col-md-9 mb-2 mb-md-0">
-                  <input
+                  {/* <input
                     type="email"
                     className="form-control form-control-lg"
                     placeholder={`${placeholder}`}
-                  />
+                  /> 
                 </div>
                 <div className="col-12 col-md-3">
-                  <button
-                    type="submit"
-                    className="btn btn-block btn-lg btn-primary"
-                  >
-                    {buttonText}
-                  </button>
+                  
                 </div>
               </div>
-            </form>
+            </form> */}
           </div>
         </div>
       </div>
