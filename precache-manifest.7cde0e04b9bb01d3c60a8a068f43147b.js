05a44e22f3b80ef2604bb2517517193208c37099
akshatsinghkaushik <37283605+akshatsinghkaushik@users.noreply.github.com>
2021-04-28 02:35:13 +0000

    Deploying to gh-pages from  @ a2a8f0eb1b56ea879475389b3031f427b02d1f38 🚀

---
+++ precache-manifest.7cde0e04b9bb01d3c60a8a068f43147b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87632fae342855894999d0634670fc26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a15bcc7855f3fd53a0d6",
+    "url": "/static/css/main.99f9b66f.chunk.css"
+  },
+  {
+    "revision": "0f6117b8bc4335cff4ba",
+    "url": "/static/js/2.a367a26a.chunk.js"
+  },
+  {
+    "revision": "08d00c1f548096a7fcc9931030de7012",
+    "url": "/static/js/2.a367a26a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15bcc7855f3fd53a0d6",
+    "url": "/static/js/main.02ad68d5.chunk.js"
+  },
+  {
+    "revision": "dfe3d817a9ac0c05d2c9",
+    "url": "/static/js/runtime-main.84cfcdfc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WebSVF/precache-manifest.c873682d72eb01b2de446f3ebdceee58.js"
+  "/precache-manifest.7cde0e04b9bb01d3c60a8a068f43147b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WebSVF/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
