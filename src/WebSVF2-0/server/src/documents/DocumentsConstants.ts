20d8e845ea6e21f01790ac39ceea5d2dd4423b9e
Rene Cruces <rene.cruces@outlook.com>
2021-04-29 11:55:21 +1000

    Initial back-end changes to return Mongo DB projects; Pending to implement saving options

---
+++ src/WebSVF2-0/server/src/documents/DocumentsConstants.ts
@@ -0,0 +1,5 @@
+export class DocumentsConstants {
+  public static readonly FileDocument = 'File';
+  public static readonly FolderDocument = 'Folder';
+  public static readonly ProjectDocument = 'Project';
+}
--- src/WebSVF2-0/server/src/index.ts
+++ src/WebSVF2-0/server/src/index.ts
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose';
 import config from './serverconfig.json';
-import userCode from './routes/userCode';
-import db from './routes/db';
 import builds from './routes/builds';
 import analysis from './routes/analysis';
 import expressFactory from './express/expressFactory';
+import projects from './routes/projects';
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
@@ -13,4 +12,4 @@ mongoose.connect(config.mongoURI);
 
 console.log('__dirname', __dirname);
 
-expressFactory({ routes: [userCode, db, builds, analysis] });
+expressFactory({ routes: [projects, builds, analysis] });
