78350d34526b9d20d6e8cf020794bf206e094844
Rene Cruces <rene.cruces@outlook.com>
2021-05-14 01:56:30 +1000

    Removed workspace, added .vscode instead

--- src/WebSVF2-0/server/src/mock.ts
+++ src/WebSVF2-0/server/src/mock.ts
@@ -1,4 +1,6 @@
 import expressFactory from './express/expressFactory';
+import files from './routes/files';
+import projects from './routes/projects';
 import analysis from './_mocks/routes/analysis';
 
-expressFactory({ routes: [analysis] });
+expressFactory({ routes: [analysis, projects, files] });
