684d6e7fc85d9727179f6bbe7216bc211ffb70ce
Rene Cruces <rene.cruces@outlook.com>
2021-09-27 08:23:17 +1000

    Changes

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { ThemeProvider as MuiThemeProvider, Theme as MuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider as StyledThemeProvider, css } from 'styled-components';
 import NoSsr from '@material-ui/core/NoSsr';
 
 interface IThemeProviderProps {
   theme: MuiTheme;
 }
 
+const globalCss = css`
+  *,
+  :after,
+  :before {
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`${globalCss}`;
+
 export const ThemeProvider: React.FC<IThemeProviderProps> = ({ children, theme }) => {
   return (
     <NoSsr>
       <MuiThemeProvider theme={theme}>
-        <StyledThemeProvider theme={theme}>{children}</StyledThemeProvider>
+        <StyledThemeProvider theme={theme}>
+          <GlobalStyle />
+          {children}
+        </StyledThemeProvider>
       </MuiThemeProvider>
     </NoSsr>
   );
